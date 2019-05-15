2fa0195f8cf4d3bb11634291f96454fbe4a336ad
melchiorhering <46674214+melchiorhering@users.noreply.github.com>
2019-05-15 13:49:33 +0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -10,6 +10,41 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from mpl_toolkits.mplot3d import Axes3D
 
+def create_similarity_matrix_categories(matrix):
+    """Create a  """
+    npu = matrix.values
+    m1 = npu @ npu.T
+    diag = np.diag(m1)
+    m2 = m1 / diag
+    m3 = np.minimum(m2, m2.T)
+    return pd.DataFrame(m3, index = matrix.index, columns = matrix.index)
+
+def extract_categoriën(bedrijven):
+    """Create an unfolded genre dataframe. Unpacks genres seprated by a '|' into seperate rows.
+
+    Arguments:
+    movies -- a dataFrame containing at least the columns 'movieId' and 'genres' 
+              where genres are seprated by '|'
+    """
+    categories_b = bedrijven.apply(lambda row: pd.Series([row['business_id']] + row['categories'].lower().split(",")), axis=1)
+    stack_categories = categories_b.set_index(0).stack()
+    df_stack_categories = stack_categories.to_frame()
+    df_stack_categories['business_id'] = stack_categories.index.droplevel(1)
+    df_stack_categories.columns = ['categories', 'business_id']
+    return df_stack_categories.reset_index()[['business_id', 'categories']]
+
+def pivot_categoriën(df):
+    """Create a one-hot encoded matrix for genres.
+    
+    Arguments:
+    df -- a dataFrame containing at least the columns 'movieId' and 'genre'
+    
+    Output:
+    a matrix containing '0' or '1' in each cell.
+    1: the movie has the genre
+    0: the movie does not have the genre
+    """
+    return df.pivot_table(index = 'business_id', columns = 'categories', aggfunc = 'size', fill_value=0)
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -24,45 +59,39 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
+    """
+    Similarity matrix aan de hand van categorieën
+    """ 
+    print(city)    
+    # Dataframe van bedrijven van een stad
+    bedrijf = DataFrame(BUSINESSES[city])
+    # Dataframe van reviews van gebruikers per bedrijf in stad
+    # user_reviews = DataFrame(REVIEWS[city])
+    # # Dataframe van users per stad
+    # users = DataFrame(USERS[city])
+
+    # Zet de catogries los van de bedrijven
+    df_categories = extract_categoriën(bedrijf)
+    print(df_categories[:10])
+
+    # Pivot de dataframe
+    df_utility_categories = pivot_categoriën(df_categories)
+    print(df_utility_categories.head())
+
+    # Maak similarity Matrix
+    df_similarity_categories = create_similarity_matrix_categories(df_utility_categories)
+    print(df_similarity_categories.head())
+
+    topvijf = REVIEWS[REVIEWS["user_id"] == user_id]
+    print(topvijf[:5])
+
+
+    """END"""
+
     if not city:
         city = random.choice(CITIES)
     return random.sample(BUSINESSES[city], n)
 
-def extract_genres(bedrijven):
-    """Create an unfolded genre dataframe. Unpacks genres seprated by a '|' into seperate rows.
-
-    Arguments:
-    movies -- a dataFrame containing at least the columns 'movieId' and 'genres' 
-              where genres are seprated by '|'
-    """
-    categories_b = bedrijven.apply(lambda row: pd.Series([row['business_id']] + row['categories'].lower().split(",")), axis=1)
-    stack_categories = categories_b.set_index(0).stack()
-    df_stack_categories = stack_categories.to_frame()
-    df_stack_categories['business_id'] = stack_categories.index.droplevel(1)
-    df_stack_categories.columns = ['categories', 'business_id']
-    return df_stack_categories.reset_index()[['business_id', 'categories']]
-
-def pivot_genres(df):
-    """Create a one-hot encoded matrix for genres.
-    
-    Arguments:
-    df -- a dataFrame containing at least the columns 'movieId' and 'genre'
-    
-    Output:
-    a matrix containing '0' or '1' in each cell.
-    1: the movie has the genre
-    0: the movie does not have the genre
-    """
-    return df.pivot_table(index = 'business_id', columns = 'categories', aggfunc = 'size', fill_value=0)
-
-def create_similarity_matrix_categories(matrix):
-    """Create a  """
-    npu = matrix.values
-    m1 = npu @ npu.T
-    diag = np.diag(m1)
-    m2 = m1 / diag
-    m3 = np.minimum(m2, m2.T)
-    return pd.DataFrame(m3, index = matrix.index, columns = matrix.index)
 
 def predict_ratings(similarity, utility, to_predict):
     """Predicts the predicted rating for the input test data.
@@ -129,58 +158,31 @@ def pivot_data(matrix):
             pivot_data[m_id, u_id] = rating 
     return pivot_data
 
-
-# def pivot_ratings(ratings):
-#     """ takes a rating table as input and computes the utility matrix """
-#     # get business and user id's
-#     business_ids = ratings['business_id'].unique()
-#     user_ids = ratings['user_id'].unique()
-#     # create empty data frame
-#     pivot_data = pd.DataFrame(ratings, columns=business_ids, index=user_ids, dtype=float)
-#     # use the function get_rating to fill the matrix
-#     for u_id in user_ids:
-#         for m_id in buiness_ids:
-#             pivot_data.loc[m_id, u_id] = get_rating(ratings, u_id, m_id) 
-#     return pivot_data
-
-
-
-
-
-
-
-# Dataframe van bedrijven van een stad
 bedrijf = DataFrame(BUSINESSES['westlake'])
-# Dataframe van reviews van gebruikers per bedrijf in stad
-user_reviews = DataFrame(REVIEWS['westlake'])
-# Dataframe van users per stad
-users = DataFrame(USERS['westlake'])
-
-"""
-Similarity matrix aan de hand van categorieën
-""" 
 # Zet de catogries los van de bedrijven
-df_categories = extract_genres(bedrijf)
-# print(df_categories[:10])
+df_categories = extract_categoriën(bedrijf)
+print(df_categories[:10])
 
 # Pivot de dataframe
-df_utility_categories = pivot_genres(df_categories)
-# print(df_utility_categories.head())
+df_utility_categories = pivot_categoriën(df_categories)
+print(df_utility_categories.head())
 
 # Maak similarity Matrix
 df_similarity_categories = create_similarity_matrix_categories(df_utility_categories)
-# print(df_similarity_categories.head())
+print(df_similarity_categories.head())
 
-"""END"""
-
+# topvijf = REVIEWS[REVIEWS["user_id"] == user_id]
+# print(topvijf[:5])
 
 """ Similarity matrix aan de hand van rating van gebruiker per bedrijf """
 
+user_reviews = DataFrame(REVIEWS['westlake'])
 # Filter user_reviews op business_id, review_id, stars en useful
 user_reviews_filtered = DataFrame(user_reviews, columns=['business_id', 'user_id','stars', 'useful'])
-# print(user_reviews_filtered)
+print(user_reviews.head())
+print(user_reviews_filtered.head())
 
 # utility_matrix = user_reviews_filtered.pivot_table(values='stars', index='business_id', columns='user_id', fill_value= 0.0)
 # print(utility_matrix)
 
-pivot_data(user_reviews_filtered)
+# pivot_data(user_reviews_filtered)
