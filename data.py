ff3c890b1bda84d2c74376c013eb17553d3959a1
Isaali <isaali.kirca@hotmail.com>
2019-05-16 13:06:42 +0200

    Werkend

--- data.py
+++ data.py
@@ -10,9 +10,12 @@ Do ensure these functions remain functional:
 import os
 import json
 import random
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+import sklearn.metrics.pairwise as pw
 
 DATA_DIR = "data"
-
 
 def load_cities():
     """
@@ -20,7 +23,6 @@ def load_cities():
     Returns a list of city names
     """
     return os.listdir(DATA_DIR)
-
 
 def load(cities, data_filename):
     """
@@ -42,7 +44,6 @@ def load(cities, data_filename):
         data[city] = city_data
     return data
 
-
 def get_business(city, business_id):
     """
     Given a city name and a business id, return that business's data.
@@ -58,7 +59,6 @@ def get_business(city, business_id):
         if business["business_id"] == business_id:
             return business
     raise IndexError(f"invalid business_id {business_id}")
-
 
 def get_reviews(city, business_id=None, user_id=None, n=10):
     """
@@ -82,7 +82,6 @@ def get_reviews(city, business_id=None, user_id=None, n=10):
     reviews = [review for review in reviews if should_keep(review)]
     return random.sample(reviews, min(n, len(reviews)))
 
-
 def get_user(username):
     """
     Get a user by its username
@@ -99,10 +98,131 @@ def get_user(username):
                 return user
     raise IndexError(f"invalid username {username}")
 
-
 CITIES = load_cities()
 USERS = load(CITIES, "user")
 BUSINESSES = load(CITIES, "business")
 REVIEWS = load(CITIES, "review")
 TIPS = load(CITIES, "tip")
 CHECKINS = load(CITIES, "checkin")
+
+def split_data(data, d = 0.75):
+    """Split data in a training and test set.
+    
+    Arguments:
+    data -- any dataFrame.
+    d    -- the fraction of data in the training set
+    """
+    np.random.seed(seed=5)
+    mask_test = np.random.rand(data.shape[0]) < d
+    return data[mask_test], data[~mask_test]
+
+def pivot_ratings(df):
+    """Creates a utility matrix for user ratings for movies
+    
+    Arguments:
+    df -- a dataFrame containing at least the columns 'movieId' and 'genres'
+    
+    Output:
+    a matrix containing a rating in each cell. np.nan means that the user did not rate the movie
+    """
+    return df.pivot_table(values='stars', columns='user_id', index='business_id')
+
+def create_similarity_matrix_cosine(matrix):
+    """Creates a adjusted(/soft) cosine similarity matrix.
+    
+    Arguments:
+    matrix -- a utility matrix
+    
+    Notes:
+    Missing values are set to 0. This is technically not a 100% correct, but is more convenient 
+    for computation and does not have a big effect on the outcome.
+    """
+    mc_matrix = matrix - matrix.mean(axis = 0)
+    return pd.DataFrame(pw.cosine_similarity(mc_matrix.fillna(0)), index = matrix.index, columns = matrix.index)
+
+def predict_ratings(similarity, utility, to_predict):
+    """Predicts the predicted rating for the input test data.
+    
+    Arguments:
+    similarity -- a dataFrame that describes the similarity between items
+    utility    -- a dataFrame that contains a rating for each user (columns) and each movie (rows). 
+                  If a user did not rate an item the value np.nan is assumed. 
+    to_predict -- A dataFrame containing at least the columns movieId and userId for which to do the predictions
+    """
+    # copy input (don't overwrite)
+    ratings_test_c = to_predict.copy()
+    # apply prediction to each row
+    ratings_test_c['predicted stars'] = to_predict.apply(lambda row: predict_ids(similarity, utility, row['user_id'], row['business_id']), axis=1)
+    return ratings_test_c
+
+### Helper functions for predict_ratings_item_based ###
+
+def predict_ids(similarity, utility, userId, itemId):
+    # select right series from matrices and compute
+    if userId in utility.columns and itemId in similarity.index:
+        return predict_vectors(utility.loc[:,userId], similarity[itemId])
+    return 0
+
+def predict_vectors(user_ratings, similarities):
+    # select only movies actually rated by user
+    relevant_ratings = user_ratings.dropna()
+    
+    # select corresponding similairties
+    similarities_s = similarities[relevant_ratings.index]
+    
+    # select neighborhood
+    similarities_s = similarities_s[similarities_s > 0.0]
+    relevant_ratings = relevant_ratings[similarities_s.index]
+    
+    # if there's nothing left return a prediction of 0
+    norm = similarities_s.sum()
+    if(norm == 0):
+        return 0
+    
+    # compute a weighted average (i.e. neighborhood is all) 
+    return np.dot(relevant_ratings, similarities_s)/norm
+
+def extract_genres(movies):
+    """Create an unfolded genre dataframe. Unpacks genres seprated by a '|' into seperate rows.
+
+    Arguments:
+    movies -- a dataFrame containing at least the columns 'movieId' and 'genres' 
+              where genres are seprated by '|'
+    """
+    genres_m = movies.apply(lambda row: pd.Series([row['business_id']] + row['categories'].lower().split(",")), axis=1)
+    stack_genres = genres_m.set_index(0).stack()
+    df_stack_genres = stack_genres.to_frame()
+    df_stack_genres['business_id'] = stack_genres.index.droplevel(1)
+    df_stack_genres.columns = ['categories', 'business_id']
+    return df_stack_genres.reset_index()[['business_id', 'categories']]
+
+def pivot_genres(df):
+    """Create a one-hot encoded matrix for genres.
+    
+    Arguments:
+    df -- a dataFrame containing at least the columns 'movieId' and 'genre'
+    
+    Output:
+    a matrix containing '0' or '1' in each cell.
+    1: the movie has the genre
+    0: the movie does not have the genre
+    """
+    return df.pivot_table(index = 'business_id', columns = 'categories', aggfunc = 'size', fill_value=0)
+
+def create_similarity_matrix_categories(matrix):
+    """Create a  """
+    npu = matrix.values
+    m1 = npu @ npu.T
+    diag = np.diag(m1)
+    m2 = m1 / diag
+    m3 = np.minimum(m2, m2.T)
+    return pd.DataFrame(m3, index = matrix.index, columns = matrix.index)
+
+def mse(predicted_ratings):
+    """Computes the mean square error between actual ratings and predicted ratings
+    
+    Arguments:
+    predicted_ratings -- a dataFrame containing the columns rating and predicted rating
+    """
+    diff = predicted_ratings['stars'] - predicted_ratings['predicted stars']
+    return (diff**2).mean()
--- recommender.py
+++ recommender.py
@@ -1,4 +1,6 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
+from data import split_data, create_similarity_matrix_cosine, predict_ratings, pivot_ratings
+from data import extract_genres, pivot_genres, create_similarity_matrix_categories, mse
 
 import random
 import json
@@ -9,42 +11,9 @@ np.seterr(divide='raise', over='raise', under='raise', invalid='raise')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from mpl_toolkits.mplot3d import Axes3D
+import sklearn.metrics.pairwise as pw
 
-def create_similarity_matrix_categories(matrix):
-    """Create a  """
-    npu = matrix.values
-    m1 = npu @ npu.T
-    diag = np.diag(m1)
-    m2 = m1 / diag
-    m3 = np.minimum(m2, m2.T)
-    return pd.DataFrame(m3, index = matrix.index, columns = matrix.index)
-
-def extract_categoriën(bedrijven):
-    """Create an unfolded genre dataframe. Unpacks genres seprated by a '|' into seperate rows.
-
-    Arguments:
-    movies -- a dataFrame containing at least the columns 'movieId' and 'genres' 
-              where genres are seprated by '|'
-    """
-    categories_b = bedrijven.apply(lambda row: pd.Series([row['business_id']] + row['categories'].lower().split(",")), axis=1)
-    stack_categories = categories_b.set_index(0).stack()
-    df_stack_categories = stack_categories.to_frame()
-    df_stack_categories['business_id'] = stack_categories.index.droplevel(1)
-    df_stack_categories.columns = ['categories', 'business_id']
-    return df_stack_categories.reset_index()[['business_id', 'categories']]
-
-def pivot_categoriën(df):
-    """Create a one-hot encoded matrix for genres.
-    
-    Arguments:
-    df -- a dataFrame containing at least the columns 'movieId' and 'genre'
-    
-    Output:
-    a matrix containing '0' or '1' in each cell.
-    1: the movie has the genre
-    0: the movie does not have the genre
-    """
-    return df.pivot_table(index = 'business_id', columns = 'categories', aggfunc = 'size', fill_value=0)
+# Content based gebruiken indien er weinig ratings beschikbaar zijn.
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -59,130 +28,56 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
-    """
-    Similarity matrix aan de hand van categorieën
-    """ 
-    print(city)    
-    # Dataframe van bedrijven van een stad
-    bedrijf = DataFrame(BUSINESSES[city])
-    # Dataframe van reviews van gebruikers per bedrijf in stad
-    # user_reviews = DataFrame(REVIEWS[city])
-    # # Dataframe van users per stad
-    # users = DataFrame(USERS[city])
+    print(user_id, business_id, city)
+    if user_id is not None:
+        if city is not None:
+            
+            reviews = DataFrame(REVIEWS[city])
+            df_reviews = reviews[['user_id', 'business_id', 'stars']]
+            df_stars_training, df_stars_test = split_data(df_reviews, d=0.9)
 
-    # Zet de catogries los van de bedrijven
-    df_categories = extract_categoriën(bedrijf)
-    print(df_categories[:10])
+            df_utility_stars = pivot_ratings(df_stars_training)
 
-    # Pivot de dataframe
-    df_utility_categories = pivot_categoriën(df_categories)
-    print(df_utility_categories.head())
+            df_similarity_stars = create_similarity_matrix_cosine(df_utility_stars)
 
-    # Maak similarity Matrix
-    df_similarity_categories = create_similarity_matrix_categories(df_utility_categories)
-    print(df_similarity_categories.head())
-
-    topvijf = REVIEWS[REVIEWS["user_id"] == user_id]
-    print(topvijf[:5])
+            predictions = predict_ratings(df_similarity_stars, df_utility_stars, df_stars_test[['user_id', 'business_id', 'stars']])
 
 
-    """END"""
+            business = DataFrame(BUSINESSES[city])
+            df_business = business[['business_id', 'name', 'categories']]
+
+            df_genres = extract_genres(df_business)
+
+            df_utility_genres = pivot_genres(df_genres)
+            
+            df_similarity_genres = create_similarity_matrix_categories(df_utility_genres)
+            
+            predicted = predict_ratings(df_similarity_genres, df_utility_stars, df_stars_test[['user_id', 'business_id', 'stars']])
+
+            mse_item_based = mse(predictions)
+            mse_content_based = mse(predicted)
+            print(mse_item_based, mse_content_based)
 
     if not city:
         city = random.choice(CITIES)
+        # print(random.sample(BUSINESSES[city], n))
     return random.sample(BUSINESSES[city], n)
 
 
-def predict_ratings(similarity, utility, to_predict):
-    """Predicts the predicted rating for the input test data.
-    
-    Arguments:
-    similarity -- a dataFrame that describes the similarity between items
-    utility    -- a dataFrame that contains a rating for each user (columns) and each business (rows). 
-                  If a user did not rate an item the value np.nan is assumed. 
-    to_predict -- A dataFrame containing at least the columns business_id and userId for which to do the predictions
-    """
-    # copy input (don't overwrite)
-    ratings_test_c = to_predict.copy()
-    # apply prediction to each row
-    ratings_test_c['predicted rating'] = to_predict.apply(lambda row: predict_ids(similarity, utility, row['userId'], row['business_id']), axis=1)
-    return ratings_test_c
+# df_users = DataFrame(USERS[city])
+# df_most_rated = df_users[df_users['review_count'] > 10]
+# most_rated_users = [i for i in df_most_rated['user_id']] 
 
-### Helper functions for predict_ratings_item_based ###
+# a = DataFrame(BUSINESSES[city])
+# b = a[a['business_id'] == business_id]
+# rating = b['stars'].values[0]
 
-def predict_ids(similarity, utility, userId, itemId):
-    # select right series from matrices and compute
-    if userId in utility.columns and itemId in similarity.index:
-        return predict_vectors(utility.loc[:,userId], similarity[itemId])
-    return 0
-
-def predict_vectors(user_ratings, similarities):
-
-    # select only movies actually rated by user
-    relevant_ratings = user_ratings.dropna()
-    
-    # select corresponding similairties
-    similarities_s = similarities[relevant_ratings.index]
-    
-    # select neighborhood
-    similarities_s = similarities_s[similarities_s > 0.0]
-    relevant_ratings = relevant_ratings[similarities_s.index]
-    
-    # if there's nothing left return a prediction of 0
-    norm = similarities_s.sum()
-    if(norm == 0):
-        return 0
-    
-    # compute a weighted average (i.e. neighborhood is all) 
-    return np.dot(relevant_ratings, similarities_s)/norm
+# c = a[a['stars'] >= rating]
+# d = c[c['review_count'] > 10] 
+# e = [i for i in d['business_id']]
 
 
-def get_rating(ratings, userId, businessId):
-    """Given a userId and businessId, this function returns the corresponding rating.
-       Should return NaN if no rating exists."""
-    rating = ratings.loc[(ratings['user_id'] == userId) & (ratings['business_id'] == businessId)]
-    if rating.empty is True:
-        return 0.0
-    return rating['stars'].item()
+# y = x[['user_id', 'business_id', 'stars']]
+# z = y[y['business_id'].isin(e)]
+# h = z[z['user_id'].isin(most_rated_users)]
 
-def pivot_data(matrix):
-
-    business_ids = matrix['business_id'].unique()
-    user_ids = matrix['user_id'].unique()
-    pivot_data = DataFrame(matrix, index=business_ids, columns=user_ids)
-    print(pivot_data)
-    for u_id in user_ids:
-        for m_id in business_ids:
-            rating = get_rating(matrix, u_id, m_id)
-            print(rating)
-            pivot_data[m_id, u_id] = rating 
-    return pivot_data
-
-bedrijf = DataFrame(BUSINESSES['westlake'])
-# Zet de catogries los van de bedrijven
-df_categories = extract_categoriën(bedrijf)
-print(df_categories[:10])
-
-# Pivot de dataframe
-df_utility_categories = pivot_categoriën(df_categories)
-print(df_utility_categories.head())
-
-# Maak similarity Matrix
-df_similarity_categories = create_similarity_matrix_categories(df_utility_categories)
-print(df_similarity_categories.head())
-
-# topvijf = REVIEWS[REVIEWS["user_id"] == user_id]
-# print(topvijf[:5])
-
-""" Similarity matrix aan de hand van rating van gebruiker per bedrijf """
-
-user_reviews = DataFrame(REVIEWS['westlake'])
-# Filter user_reviews op business_id, review_id, stars en useful
-user_reviews_filtered = DataFrame(user_reviews, columns=['business_id', 'user_id','stars', 'useful'])
-print(user_reviews.head())
-print(user_reviews_filtered.head())
-
-# utility_matrix = user_reviews_filtered.pivot_table(values='stars', index='business_id', columns='user_id', fill_value= 0.0)
-# print(utility_matrix)
-
-# pivot_data(user_reviews_filtered)
